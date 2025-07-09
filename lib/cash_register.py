b4acfa9198f6b1a53a564bc84b444ca466342a7a
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-09 12:19:39 -0600

    fixed apply discount function to pass test

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -29,19 +29,19 @@ class CashRegister:
     })
 
   def apply_discount(self):
-    if not self.items or self.discount == 0:
-      print(f"There is no discount to apply.\n")
-      return
-    discount_amount = (self.discount / 100) * self.total 
-    self.total = round(self.total - discount_amount, 2)
-    print(f"After the discount, the total comes to ${self.total:.0f}.")
+    if not self.prev_transactions or not self.discount:
+      print("There is no discount to apply.")
+    else:
+      discount_amount = (self.discount / 100) * self.total 
+      self.total = round(self.total - discount_amount, 2)
+      print(f"After the discount, the total comes to ${self.total:.0f}.")
     
   def void_last_transaction(self):
     if not self.prev_transactions:
       print(f"There are no transactions to void")
-      return
-    last_transaction = self.prev_transactions.pop()
-    self.total -= last_transaction["price"] * last_transaction["quantity"]
+    else:
+      last_transaction = self.prev_transactions.pop()
+      self.total -= last_transaction["price"] * last_transaction["quantity"]
     
 
 
